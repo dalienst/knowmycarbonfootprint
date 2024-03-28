c1186e206e492852df022e19e582277c41c70401
dalienst <oduordalienst@gmail.com>
2024-03-28 06:51:19 +0300

    change urls

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,9 +1,10 @@
+/* eslint-disable no-unused-vars */
 import axios from "axios";
 
 const LOCAL_URL = "http://127.0.0.1:8000/carbon/";
-const API_URL = "";
+const API_URL = "https://climate-carbon-footprint.onrender.com/";
 
 export const api = axios.create({
-  baseURL: LOCAL_URL,
+  baseURL: API_URL,
   headers: { "Content-Type": "application/json" },
 });
--- src/data/homeData.jsx
+++ src/data/homeData.jsx
@@ -7,7 +7,7 @@ const homeData = [
   {
     id: 2,
     title: "Climate Change Mitigation",
-    content: `By reducing carbon footprints, individuals can play a significant role in mitigating climate change and minimizing its harmful effects on ecosystems, weather patterns, and global temperatures`,
+    content: `Carbon footprints directly contribute to greenhouse gas emissions, which are the primary drivers of climate change. By reducing carbon footprints, individuals can play a significant role in mitigating climate change and minimizing its harmful effects on ecosystems, weather patterns, and global temperatures.`,
   },
   {
     id: 3,
@@ -16,6 +16,26 @@ const homeData = [
   },
   {
     id: 4,
+    title: "Biodiversity Preservation",
+    content: `Climate change and habitat destruction resulting from carbon emissions threaten biodiversity by altering ecosystems and disrupting the habitats of plant and animal species. By minimizing carbon footprints, individuals can help protect biodiversity and preserve ecosystems, promoting the health and resilience of natural habitats and the species that depend on them.`,
+  },
+  {
+    id: 5,
+    title: "Resource Conservation",
+    content: `: High carbon footprints often correlate with high resource consumption, including energy, water, and raw materials. By reducing carbon footprints, individuals can help conserve finite resources and promote more sustainable use of natural resources, ensuring their availability for future generations.`,
+  },
+  {
+    id: 6,
+    title: "Social Equity",
+    content: `Climate change disproportionately affects vulnerable populations, including low-income communities, indigenous peoples, and marginalized groups. By addressing carbon footprints and promoting sustainable practices, individuals can contribute to building more resilient and equitable societies, where all people have access to clean air, water, and resources.`,
+  },
+  {
+    id: 7,
+    title: "Economic Savings",
+    content: `Adopting sustainable practices and reducing carbon footprints can lead to cost savings for individuals and businesses through lower energy bills, reduced waste disposal costs, and increased efficiency. By embracing sustainable solutions, individuals can contribute to building a more prosperous and resilient economy that prioritizes environmental stewardship and long-term sustainability.`,
+  },
+  {
+    id: 8,
     title: "Global Responsibility",
     content: `Climate change is a global challenge that requires collective action and cooperation at all levels, from individuals and communities to governments and international organizations. By taking responsibility for their carbon footprints and advocating for sustainable policies and practices, individuals can contribute to a global movement for positive change and create a more sustainable future for all.`,
   },
